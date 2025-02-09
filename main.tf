f72948106146acebb81ca0d310c1120538993275
SRINU42 <srinu.karnam01@gmail.com>
2025-02-09 19:25:40 +0530

    a

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "instance" {
-    for_each = var.component
-    source = "git::https://github.com/SRINU42/app-tfmodule.git"
-    component = each.key
-    env = var.env
+    for_each    = var.component
+    source      = "git::https://github.com/SRINU42/app-tfmodule.git"
+    component   = each.key
+    env         = var.env
+    tags        = each.value["tags"]
 }
