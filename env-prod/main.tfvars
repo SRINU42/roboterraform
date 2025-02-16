07625b099e66f8464b8fdc321ead34aae2949357
SRINU42 <srinu.karnam01@gmail.com>
2025-02-16 22:23:46 +0530

    vpc

--- env-prod/main.tfvars
+++ env-prod/main.tfvars
@@ -1,1 +1,14 @@
 env = "prod"
+tags = {
+    company_name  = "xyz Tech"
+    Business      = "Ecommerce"
+    Business_unit = "Retail"
+    Cost_center   = "322" 
+    Project_name  = "Roboshop"  
+}
+
+vpc = {
+    main = {
+        cidr_block = "10.20.0.0/16"
+    }
+}
--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
-module "instance" {
-    for_each    = var.component
-    source      = "git::https://github.com/SRINU42/app-tfmodule.git"
-    component   = each.key
+##module "instance" {
+##    for_each    = var.component
+##    source      = "git::https://github.com/SRINU42/app-tfmodule.git"
+##    component   = each.key
+##    env         = var.env
+##    tags        = merge(each.value["tags"], var.tags)
+##}
+
+module "vpc" {
+
+    source      = "git::https://github.com/SRINU42/vpc-tfmodule.git"
+    
+    for_each    = var.vpc
+    cidr_block  = each.value["cidr_block"]
+    subnets = each.value["sub"]
+
     env         = var.env
-    tags        = merge(each.value["tags"], var.tags)
+    tags        = var.tags  
 }
